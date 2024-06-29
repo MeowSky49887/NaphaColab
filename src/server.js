7d67b738612480fbcc8cc38f33656df713ce31e3
MeowSky49887 <143077173+MeowSky49887@users.noreply.github.com>
2024-06-30 01:22:51 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,95 +4,76 @@ const cors = require('cors');
 const favicon = require('serve-favicon');
 const path = require('path');
 const utils = require('./utils');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
-// fn to create express server
 const create = async () => {
-
-    // server
     const app = express();
+
     app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
-    
-    // Log request
     app.use(utils.appLogger);
+    app.use(cors());
+    app.use(bodyParser.json());
 
-    // Enable CORS
-    app.use(cors()); // Enable CORS for all routes
-
-    app.use(bodyParser.json()) // for parsing application/json
-
-    // root route - serve static file
-    app.get('/APIs', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
+    app.get('/APIs', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/client.html'));
+    });
 
     app.get('/*', (req, res) => {
-        var url;
-        
-        if (req.originalUrl == 'llama') {
-            url = 'https://napha-llama.loca.lt' + req.originalUrl.replace("/llama", ""); // Append the original URL path to the base URL
-        } else if (req.originalUrl == 'voicevox') {
-            url = 'https://napha-voicevox.loca.lt' + req.originalUrl.replace("/voicevox", ""); // Append the original URL path to the base URL
+        let url;
+
+        if (req.originalUrl.includes('/llama')) {
+            url = 'https://napha-llama.loca.lt' + req.originalUrl.replace("/llama", "");
+        } else if (req.originalUrl.includes('/voicevox')) {
+            url = 'https://napha-voicevox.loca.lt' + req.originalUrl.replace("/voicevox", "");
         }
-    
-        axios.get(url, req.body,{
+
+        axios.get(url, {
             headers: {
                 'bypass-tunnel-reminder': 'true',
                 'Content-Type': 'application/json'
             }
         }).then(response => {
-            // Set the headers from the external response
             Object.keys(response.headers).forEach(header => {
                 res.setHeader(header, response.headers[header]);
             });
-    
-            // Send the status and data from the external response
             res.status(response.status).send(response.data);
         }).catch(error => {
-            // Handle errors appropriately
             if (error.response) {
-                // If the error is from the external request, forward the status and message
                 res.status(error.response.status).send(error.response.data);
             } else {
-                // If the error is in making the request, send a 500 status
                 res.status(500).send(error.message);
             }
         });
     });
 
     app.post('/*', (req, res) => {
-        var url;
-        
-        if (req.originalUrl == 'llama') {
-            url = 'https://napha-llama.loca.lt' + req.originalUrl.replace("/llama", ""); // Append the original URL path to the base URL
-        } else if (req.originalUrl == 'voicevox') {
-            url = 'https://napha-voicevox.loca.lt' + req.originalUrl.replace("/voicevox", ""); // Append the original URL path to the base URL
+        let url;
+
+        if (req.originalUrl.includes('/llama')) {
+            url = 'https://napha-llama.loca.lt' + req.originalUrl.replace("/llama", "");
+        } else if (req.originalUrl.includes('/voicevox')) {
+            url = 'https://napha-voicevox.loca.lt' + req.originalUrl.replace("/voicevox", "");
         }
 
-        axios.post(url, req.body,{
+        axios.post(url, req.body, {
             headers: {
                 'bypass-tunnel-reminder': 'true',
                 'Content-Type': 'application/json'
             }
         }).then(response => {
-            // Set the headers from the external response
             Object.keys(response.headers).forEach(header => {
                 res.setHeader(header, response.headers[header]);
             });
-    
-            // Send the status and data from the external response
             res.status(response.status).send(response.data);
         }).catch(error => {
-            // Handle errors appropriately
             if (error.response) {
-                // If the error is from the external request, forward the status and message
                 res.status(error.response.status).send(error.response.data);
             } else {
-                // If the error is in making the request, send a 500 status
                 res.status(500).send(error.message);
             }
         });
     });
 
-    // Catch errors
     app.use(utils.logErrors);
     app.use(utils.clientError404Handler);
     app.use(utils.clientError500Handler);
