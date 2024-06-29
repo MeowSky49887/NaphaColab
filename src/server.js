478c998bb0ecba41597cf9a2c6b34665c808c787
MeowSky49887 <143077173+MeowSky49887@users.noreply.github.com>
2024-06-30 00:27:35 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,9 +22,9 @@ const create = async () => {
     app.use(bodyParser.json()) // for parsing application/json
 
     // root route - serve static file
-    app.get('/voicevox.ipynb', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
+    app.get('/APIs.ipynb', (req, res) => res.sendFile(path.join(__dirname, '../public/client.html')));
 
-    app.get('/', (req, res) => {
+    app.get('/voicevox/', (req, res) => {
         const url = 'https://napha-voicevox.loca.lt' + req.originalUrl; // Append the original URL path to the base URL
     
         axios.get(url, req.body,{
@@ -52,7 +52,7 @@ const create = async () => {
         });
     });
 
-    app.post('/', (req, res) => {
+    app.post('/voicevox/', (req, res) => {
         const url = 'https://napha-voicevox.loca.lt' + req.originalUrl; // Append the original URL path to the base URL
     
         axios.post(url, req.body,{
@@ -80,6 +80,62 @@ const create = async () => {
         });
     });
 
+    app.get('/llama/', (req, res) => {
+        const url = 'https://napha-llama.loca.lt' + req.originalUrl; // Append the original URL path to the base URL
+    
+        axios.get(url, req.body,{
+            headers: {
+                'bypass-tunnel-reminder': 'true',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            // Set the headers from the external response
+            Object.keys(response.headers).forEach(header => {
+                res.setHeader(header, response.headers[header]);
+            });
+    
+            // Send the status and data from the external response
+            res.status(response.status).send(response.data);
+        }).catch(error => {
+            // Handle errors appropriately
+            if (error.response) {
+                // If the error is from the external request, forward the status and message
+                res.status(error.response.status).send(error.response.data);
+            } else {
+                // If the error is in making the request, send a 500 status
+                res.status(500).send(error.message);
+            }
+        });
+    });
+
+    app.post('/llama/', (req, res) => {
+        const url = 'https://napha-llama.loca.lt' + req.originalUrl; // Append the original URL path to the base URL
+    
+        axios.post(url, req.body,{
+            headers: {
+                'bypass-tunnel-reminder': 'true',
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            // Set the headers from the external response
+            Object.keys(response.headers).forEach(header => {
+                res.setHeader(header, response.headers[header]);
+            });
+    
+            // Send the status and data from the external response
+            res.status(response.status).send(response.data);
+        }).catch(error => {
+            // Handle errors appropriately
+            if (error.response) {
+                // If the error is from the external request, forward the status and message
+                res.status(error.response.status).send(error.response.data);
+            } else {
+                // If the error is in making the request, send a 500 status
+                res.status(500).send(error.message);
+            }
+        });
+    });
+    
     // Catch errors
     app.use(utils.logErrors);
     app.use(utils.clientError404Handler);
